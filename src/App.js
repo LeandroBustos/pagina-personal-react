91bf80395e2c9d250d2688e706866044c2587a0b
LeandroBustos <LeandroBustos@github.com>
2021-03-15 01:01:26 -0300

    some components well implemented although the content looks like shit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
+import autism from './autism.jpg';
 import './App.css';
+// import './normalize.css';
+// import {Container, Box} from '@material-ui/core'
+import NavBar from './components/NavBar';
+import FlexboxTest from './components/FlexboxTest.jsx'
+
+//MODULES
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <>
+    <header>
+      <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+      <NavBar/>
+    </header>
+      <FlexboxTest/>
+      <div style={{display:"flex", justifyContent:"center", alignItems:"center", marginBottom:200}}>
+        <img src={autism} alt="logo" />
+      </div>
+      <p>Todos los derechos reservados, si me robas sos puto</p>
+    </>
   );
 }
 
